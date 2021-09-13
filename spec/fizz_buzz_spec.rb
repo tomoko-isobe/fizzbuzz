8b89e87abcf5b321ba74e137bebcbb27342b4e6c
tomoko-isobe <isobe@humancrest.co.jp>
2021-09-13 20:01:04 +0900

    課題1-1 既存の実行コードのテストを追加

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -3,7 +3,18 @@ RSpec.describe FizzBuzz do
     expect(FizzBuzz::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#response" do
+    let(:fizz_buzz){ FizzBuzz.new(input_str) }
+    subject{ fizz_buzz.response }
+
+    context "1が入力されたとき" do
+      let(:input_str){ "1" }
+      it{ is_expected.to eq "1" }
+    end
+
+    context "9999が入力されたとき" do
+      let(:input_str){ "9999" }
+      it{ is_expected.to eq "9999" }
+    end
   end
 end
