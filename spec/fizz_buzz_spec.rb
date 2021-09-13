dc800369a954502d241f8cbb459b312f5c8cfefb
tomoko-isobe <isobe@humancrest.co.jp>
2021-09-13 20:53:26 +0900

    課題1-3: 3の倍数のテストコードを追加

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -8,8 +8,15 @@ RSpec.describe FizzBuzz do
     subject{ fizz_buzz.response }
 
     context "正常値: 0以上の数字" do
-      context("1が入力されたとき")   { let(:i){ "1" };    it{ is_expected.to eq "1" } }
-      context("9999が入力されたとき"){ let(:i){ "9999" }; it{ is_expected.to eq "9999" } }
+      context "0以上の数字" do
+        context("1")   { let(:i){ "1" };    it{ is_expected.to eq "1" } }
+        context("9999"){ let(:i){ "9999" }; it{ is_expected.to eq "9999" } }
+      end
+
+      context "3の倍数" do
+        context("3")   { let(:i){ "3" };    it{ is_expected.to eq "Fizz" } }
+        context("6")   { let(:i){ "6" };    it{ is_expected.to eq "Fizz" } }
+      end
     end
 
     context "異常値" do
