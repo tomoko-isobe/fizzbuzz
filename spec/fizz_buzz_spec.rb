0e88dac57e20e029952c0e5665b71bfe8e8db3dd
tomoko-isobe <isobe@humancrest.co.jp>
2021-09-13 20:42:26 +0900

    課題1-2: テストコードを読みやすく圧縮

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -4,26 +4,16 @@ RSpec.describe FizzBuzz do
   end
 
   describe "#response" do
-    let(:fizz_buzz){ FizzBuzz.new(input_str) }
+    let(:fizz_buzz){ FizzBuzz.new(i) }
     subject{ fizz_buzz.response }
 
     context "正常値: 0以上の数字" do
-      context "1が入力されたとき" do
-        let(:input_str){ "1" }
-        it{ is_expected.to eq "1" }
-      end
-
-      context "9999が入力されたとき" do
-        let(:input_str){ "9999" }
-        it{ is_expected.to eq "9999" }
-      end
+      context("1が入力されたとき")   { let(:i){ "1" };    it{ is_expected.to eq "1" } }
+      context("9999が入力されたとき"){ let(:i){ "9999" }; it{ is_expected.to eq "9999" } }
     end
 
     context "異常値" do
-      context "0以下の数字" do
-        let(:input_str){ "0" }
-        it { expect { subject }.to raise_error(InputError) }
-      end
+      context("0以下の数字") { let(:i){ "0" };  it { expect { subject }.to raise_error(InputError) }}
 
       context "数字以外の文字列"
       context "全角数字"
