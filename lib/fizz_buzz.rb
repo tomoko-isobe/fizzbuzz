4ef6d84b116325bf7821f5ee36ba12c48268871b
tomoko-isobe <isobe@humancrest.co.jp>
2021-09-13 21:00:17 +0900

    課題1-3: 実行コードを実装したら既存テストが1件Fail

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -4,17 +4,22 @@ class InputError < StandardError; end
 
 class FizzBuzz
   def initialize str
-    @input_str = str
+    @input = str.to_i
   end
 
   def response
     raise InputError unless valid?
-    @input_str
+    case
+    when (@input % 3)==0
+      "Fizz"
+    else
+      @input.to_s
+    end
   end
 
   private
   def valid?
-    @input_str.to_i > 0
+    @input > 0
   end
 
 end
