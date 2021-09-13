803480d55e6b2cf33705f2840f641a0629bdb835
tomoko-isobe <isobe@humancrest.co.jp>
2021-09-13 20:31:15 +0900

    課題1-2: 0以下を入力した場合に例外というテストを追加

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -7,14 +7,26 @@ RSpec.describe FizzBuzz do
     let(:fizz_buzz){ FizzBuzz.new(input_str) }
     subject{ fizz_buzz.response }
 
-    context "1が入力されたとき" do
-      let(:input_str){ "1" }
-      it{ is_expected.to eq "1" }
+    context "正常値: 0以上の数字" do
+      context "1が入力されたとき" do
+        let(:input_str){ "1" }
+        it{ is_expected.to eq "1" }
+      end
+
+      context "9999が入力されたとき" do
+        let(:input_str){ "9999" }
+        it{ is_expected.to eq "9999" }
+      end
     end
 
-    context "9999が入力されたとき" do
-      let(:input_str){ "9999" }
-      it{ is_expected.to eq "9999" }
+    context "異常値" do
+      context "0以下の数字" do
+        let(:input_str){ "0" }
+        it { expect { subject }.to raise_error(InputError) }
+      end
+
+      context "数字以外の文字列"
+      context "全角数字"
     end
   end
 end
